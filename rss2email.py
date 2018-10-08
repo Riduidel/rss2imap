b41f933ec063e446d30a0d5b239674560bd371f1
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2018-10-08 14:28:53 +0200

    Fixed opmlexport
    Finally, an export function I can be proud of !

--- rss2email.py
+++ rss2email.py
@@ -857,16 +857,29 @@ def list():
             print "   W: Please define a default address with 'r2e email emailaddress'"
         i+= 1
 
+
 def opmlexport():
     import xml.sax.saxutils
     feeds, feedfileObject = load(lock=0)
-    
     if feeds:
         print '<?xml version="1.0" encoding="UTF-8"?>\n<opml version="1.0">\n<head>\n<title>rss2email OPML export</title>\n</head>\n<body>'
-        for f in feeds[1:]:
-            url = xml.sax.saxutils.escape(f.url)
-            print '<outline type="rss" text="%s" xmlUrl="%s"/>' % (url, url)
-        print '</body>\n</opml>'
+        exportableFeeds = {}
+        if USE_OPML_TITLE_AS_FOLDER:
+            for f in feeds[1:]:
+                if not f.folder in exportableFeeds:
+                    exportableFeeds[f.folder] = []
+                exportableFeeds[f.folder].append(f)
+            for folder in exportableFeeds:
+                print '\n\t<outline text="%s" title="%s">' % (folder, folder)
+                for f in exportableFeeds[folder]:
+                    url = xml.sax.saxutils.escape(f.url)
+                    print '\n\t\t<outline type="rss" text="%s" xmlUrl="%s"/>' % (url, url)
+                print '\n\t</outline>'
+        else:
+            for f in feeds[1:]:
+                url = xml.sax.saxutils.escape(f.url)
+                print '<outline type="rss" text="%s" xmlUrl="%s"/>' % (url, url)
+        print '\n</body>\n</opml>'
 
 def opmlimport(importfile):
     importfileObject = None
